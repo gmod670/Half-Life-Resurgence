2cc6b51a4683510fbb2826838621210ebbdfc715
Cpt. Hazama <Caleb123459@gmail.com>
2019-09-20 17:18:11 -0400

    HD pack file setup
    - Includes spawnmenu
    - Includes Alien Grunt and Alien Slave models

--- lua/autorun/vj_hlr_hd_autorun.lua
+++ lua/autorun/vj_hlr_hd_autorun.lua
@@ -5,8 +5,8 @@
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 --------------------------------------------------*/
 ------------------ Addon Information ------------------
-local PublicAddonName = "Half-Life Resurgence"
-local AddonName = "Half-Life Resurgence"
+local PublicAddonName = "Half-Life Resurgence HD"
+local AddonName = "Half-Life Resurgence HD"
 local AddonType = "SNPC"
 local AutorunFile = "autorun/vj_hlr_hd_autorun.lua"
 -------------------------------------------------------
@@ -16,7 +16,42 @@ if VJExists == true then
 	
 	local vCat = "Half-Life Resurgence: HD"
 
-	//VJ.AddNPC("Human Grunt","npc_vj_hlr1_hgrunt",vCat)
+		-- HECU
+		VJ.AddNPC("Human Grunt","npc_vj_hlr1_hgrunt_hd",vCat)
+		VJ.AddNPC("Human Sergeant","npc_vj_hlr1_hsergeant_hd",vCat)
+			-- Opposing Force
+			VJ.AddNPC("Human Grunt (OppF)","npc_vj_hlrof_hgrunt_hd",vCat)
+			VJ.AddNPC("Human Grunt Medic (OppF)","npc_vj_hlrof_hgrunt_med_hd",vCat)
+			VJ.AddNPC("Human Grunt Engineer (OppF)","npc_vj_hlrof_hgrunt_eng_hd",vCat)
+		
+		-- Black Ops
+		VJ.AddNPC("Black Ops Male Assassin","npc_vj_hlrof_assassin_male_hd",vCat)
+		
+		-- Black Mesa Personnel
+		VJ.AddNPC("Security Guard","npc_vj_hlr1_securityguard_hd",vCat)
+		VJ.AddNPC("Scientist","npc_vj_hlr1_scientist_hd",vCat)
+			-- Opposing Force
+			VJ.AddNPC("Cleansuit Scientist","npc_vj_hlrof_cleansuitsci_hd",vCat)
+			VJ.AddNPC("Otis Laurey","npc_vj_hlrof_otis_hd",vCat)		
+		-- Xen Creatures
+		VJ.AddNPC("Alien Grunt","npc_vj_hlr1_aliengrunt_hd",vCat)
+		VJ.AddNPC("Bullsquid","npc_vj_hlr1_bullsquid_hd",vCat)
+		VJ.AddNPC("Gargantua","npc_vj_hlr1_garg_hd",vCat)
+		VJ.AddNPC("Houndeye","npc_vj_hlr1_houndeye_hd",vCat)
+		VJ.AddNPC("Alien Slave","npc_vj_hlr1_vortigaunt_hd",vCat)
+			-- Headcrab
+			VJ.AddNPC("Headcrab","npc_vj_hlr1_headcrab_hd",vCat)
+			VJ.AddNPC("Zombie","npc_vj_hlr1_zombie_hd",vCat)
+				-- Opposing Force
+				VJ.AddNPC("Zombie Security Guard","npc_vj_hlrof_zombie_sec_hd",vCat)
+				VJ.AddNPC("Zombie Soldier","npc_vj_hlrof_zombie_soldier_hd",vCat)
+				VJ.AddNPC("Gonome","npc_vj_hlrof_gonome_hd",vCat)
+		
+		-- Race X
+		VJ.AddNPC("Shock Trooper","npc_vj_hlrof_shocktrooper_hd",vCat)
+
+		-- Sven Co-Op
+		VJ.AddNPC("Baby Gargantua","npc_vj_hlrsv_babygarg_hd",vCat)
 	
 -- !!!!!! DON'T TOUCH ANYTHING BELOW THIS !!!!!! -------------------------------------------------------------------------------------------------------------------------
 	AddCSLuaFile(AutorunFile)
