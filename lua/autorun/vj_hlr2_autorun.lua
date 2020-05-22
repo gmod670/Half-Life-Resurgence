9816110e64488dfa6b5799b91895715e63bd3fdb
DrVrej <DrVrej@users.noreply.github.com>
2020-05-22 16:30:09 -0400

    Removed Custom Crossbow

--- lua/autorun/vj_hlr2_autorun.lua
+++ lua/autorun/vj_hlr2_autorun.lua
@@ -76,12 +76,12 @@ if VJExists == true then
 
 		-- Resistance
 		VJ.AddNPC_HUMAN("Alyx Vance","npc_vj_hlr2_alyx",{"weapon_vj_hl2_alyxgun"},vCat)
-		VJ.AddNPC_HUMAN("Barney Calhoun","npc_vj_hlr2_barney",{"weapon_vj_hl2_crossbow","weapon_vj_357","weapon_vj_9mmpistol","weapon_vj_glock17","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_ar2","weapon_vj_ar2","weapon_vj_spas12"},vCat)
+		VJ.AddNPC_HUMAN("Barney Calhoun","npc_vj_hlr2_barney",{"weapon_vj_crossbow","weapon_vj_357","weapon_vj_9mmpistol","weapon_vj_glock17","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_ar2","weapon_vj_ar2","weapon_vj_spas12"},vCat)
 		-- VJ.AddNPC_HUMAN("Odessa Cubbage","npc_vj_hlr2_odesa",{"weapon_vj_357","weapon_vj_9mmpistol","weapon_vj_glock17","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_ar2","weapon_vj_ar2","weapon_vj_spas12"},vCat)
 		VJ.AddNPC_HUMAN("Father Grigori","npc_vj_hlr2_father_grigori",{"weapon_vj_hl2_annabelle"},vCat)
 		VJ.AddNPC_HUMAN("Citizen","npc_vj_hlr2_citizen",{},vCat)
 		VJ.AddNPC_HUMAN("Refugee","npc_vj_hlr2_refugee",{"weapon_vj_357","weapon_vj_9mmpistol","weapon_vj_glock17","weapon_vj_smg1"},vCat)
-		VJ.AddNPC_HUMAN("Rebel","npc_vj_hlr2_rebel",{"weapon_vj_hl2_crossbow","weapon_vj_hl2_crossbow","weapon_vj_357","weapon_vj_9mmpistol","weapon_vj_glock17","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_k3","weapon_vj_k3","weapon_vj_ar2","weapon_vj_ar2","weapon_vj_ak47","weapon_vj_m16a1","weapon_vj_mp40","weapon_vj_spas12","weapon_vj_rpg","weapon_vj_blaster"},vCat)
+		VJ.AddNPC_HUMAN("Rebel","npc_vj_hlr2_rebel",{"weapon_vj_crossbow","weapon_vj_crossbow","weapon_vj_357","weapon_vj_9mmpistol","weapon_vj_glock17","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_smg1","weapon_vj_k3","weapon_vj_k3","weapon_vj_ar2","weapon_vj_ar2","weapon_vj_ak47","weapon_vj_m16a1","weapon_vj_mp40","weapon_vj_spas12","weapon_vj_rpg","weapon_vj_blaster"},vCat)
 		VJ.AddNPC("Floor Turret (Resistance)","npc_vj_hlr2_turret",vCat)
 		-- VJ.AddNPC("Vortigaunt","npc_vj_hlr2_vort",vCat)
 		VJ.AddNPC("Lamarr","npc_vj_hlr2_lamarr",vCat)
@@ -122,7 +122,6 @@ if VJExists == true then
 		VJ.AddNPCWeapon("VJ_Annabelle","weapon_vj_hl2_annabelle",false,vCat)
 		VJ.AddNPCWeapon("VJ_Alyx_Gun","weapon_vj_hl2_alyxgun",false,vCat)
 		VJ.AddNPCWeapon("VJ_OICW","weapon_vj_hl2b_oicw",false,vCat)
-		VJ.AddNPCWeapon("VJ_Crossbow","weapon_vj_hl2_crossbow",false,vCat)
 		VJ.AddNPCWeapon("VJ_Combine_Reager","weapon_vj_hlr2_reager",false,vCat)
 		VJ.AddNPCWeapon("VJ_StunStick","weapon_vj_hl2_stunstick",false,vCat)
 	
